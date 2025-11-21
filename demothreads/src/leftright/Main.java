9fafe54148dbfa73a2502a015c4f12d5f8e6b4ba
Andriy Sichniy <sichniy.andriy@gmail.com>
2025-11-21 02:53:29 +0200

    Add multithreading examples: Left-Right and Ping-Pong simulations

---
+++ demothreads/src/leftright/Main.java
@@ -0,0 +1,30 @@
+package leftright;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Walk walk = new Walk(20);
+        ThreadFactory threadFactory = Executors.defaultThreadFactory();
+
+        Thread threadLeft = threadFactory.newThread(() -> {
+            while (walk.decrementSteps() > 0) {
+                walk.stepLeft();
+            }
+        });
+        threadLeft.setName("LeftThread");
+
+        Thread threadRight = threadFactory.newThread(() -> {
+            while (walk.decrementSteps() > 0) {
+                walk.stepRight();
+            }
+        });
+        threadRight.setName("RightThread");
+
+        threadLeft.start();
+        threadRight.start();
+        threadLeft.join();
+        threadRight.join();
+    }
+}
